ec6f2f8800f685c59fabdd1ae4010ea45ab48ad1
dev <meddevlopp@gmail.com>
2017-03-13 18:03:03 +0100

    Modif

--- src/main/java/com/polytech/view/Main.java
+++ src/main/java/com/polytech/view/Main.java
@@ -14,7 +14,12 @@ public class Main {
         PublicationService publicationService = new PublicationService(postRepository);
 
         Post post = new Post("Luminy");
+        Post post1 = new Post("Etoil");
+        Post post2 = new Post("Test");
         publicationService.post(post);
+        publicationService.post(post1);
+        publicationService.post(post2);
+
         System.out.println(publicationService.fetchAll());
     }
 }
---
+++ src/test/java/com/polytech/view/MainTest.groovy
@@ -0,0 +1,7 @@
+package com.polytech.view
+
+/**
+ * Created by dev on 3/13/17.
+ */
+class MainTest extends groovy.util.GroovyTestCase {
+}
