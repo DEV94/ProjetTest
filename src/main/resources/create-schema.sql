6bda3dd79a5ca64808a2cdb7ed5ff9d78096164b
dev <meddevlopp@gmail.com>
2017-03-17 11:46:02 +0100

    Testing Functionality

--- src/main/resources/create-schema.sql
+++ src/main/resources/create-schema.sql
@@ -2,3 +2,16 @@ CREATE TABLE POST (
  ID int PRIMARY KEY AUTO_INCREMENT,
  content VARCHAR(140)
 );
+
+CREATE TABLE users(
+      username varchar_ignorecase(50) not null primary key,
+      password varchar_ignorecase(250) not null,
+      enabled boolean not null
+      );
+CREATE TABLE authorities (
+      username varchar_ignorecase(50) not null,
+      authority varchar_ignorecase(50) not null,
+      constraint fk_authorities_users foreign key(username) references users(username)
+      );
+
+      create unique index ix_auth_username on authorities (username,authority);
